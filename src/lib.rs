fc1c71ae2a82e7eaca21a7739677e66fb2e0c87b
J. Cliff Dyer <jcd@sdf.org>
2020-12-15 09:09:37 -0500

    Remove unused modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,8 @@ extern crate crypto;
 use std::result;
 pub use connection::Connection;
 
+pub mod auth;
 pub mod connection;
 pub mod error;
-pub mod message;
-pub mod servermsg;
-pub mod auth;
 
 pub type Result<T> = result::Result<T, error::PgError>;
